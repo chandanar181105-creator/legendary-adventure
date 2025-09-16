eb01e0d7299ecb7a06c02501f8dcf929534eaf77
chandanar181105-creator <chandanar181105@gmail.com>
2025-09-16 15:25:58 +0530

    Add models.py for event management Flask app

---
+++ backend/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    date = db.Column(db.String(20))
+    location = db.Column(db.String(100))
+    description = db.Column(db.Text)
